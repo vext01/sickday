eb52fa1d874350edfa13a09dc0f9326e1a2a2ef4
vext01 <vext01@gmail.com>
2012-03-20 22:15:20 +0000

    a start of a menu

--- sickday.py
+++ sickday.py
@@ -17,12 +17,15 @@ class SickGUI:
                 "bg" : (0x00, 0x00, 0x00),
                 "text" :  (0x00, 0x99, 0xff),
                 "lines" :  (0x00, 0x99, 0xff),
+                "selected_bg" :  (0x00, 0x99, 0xff),
+                "selected_text" :  (0xff, 0xff, 0x00),
         }
 
         self.fonts =  {
                 "title" : pygame.font.Font("Sony_Sketch_EF.ttf", 45),
                 "footer" : pygame.font.Font("Sony_Sketch_EF.ttf", 20),
-                "default" : pygame.font.Font("Sony_Sketch_EF.ttf", 35)
+                "default" : pygame.font.Font("Sony_Sketch_EF.ttf", 35),
+                "menu" : pygame.font.Font("Sony_Sketch_EF.ttf", 35)
         }
         
         self.title_text = ""
@@ -34,6 +37,28 @@ class SickGUI:
 
         self.screen = pygame.display.set_mode((800, 600))
 
+        # figure out how big the viewport is
+        sample1 = self.fonts["title"].render(
+                "test", True, self.colours["text"])
+        sample2 = self.fonts["footer"].render(
+                "test", True, self.colours["text"])
+        sample3 = self.fonts["menu"].render(
+                "test", True, self.colours["text"])
+
+        viewport_poss = pygame.Surface((self.screen.get_rect().w,
+                    self.screen.get_rect().h - sample1.get_rect().h -
+                    sample2.get_rect().h))
+
+        self.n_menu_items = viewport_poss.get_rect().h / sample3.get_rect().h
+
+        self.viewport = pygame.Surface((self.screen.get_rect().w, self.n_menu_items *
+            sample3.get_rect().h))
+
+        self.viewport_pos = (0, sample1.get_rect().h + ((viewport_poss.get_rect().h -
+                self.viewport.get_rect().h) / 2))
+
+        self.selected_menu_item = 0
+
     def update_gui(self):
 
         self.footer_text_right = time.strftime("%a, %d %b %Y %H:%M",
@@ -42,6 +67,7 @@ class SickGUI:
 
         # start drawing
         self.screen.fill(self.colours["bg"])
+        self.viewport.fill(self.colours["bg"])
 
         # render header and footer
         title = self.fonts["title"].render(
@@ -50,6 +76,8 @@ class SickGUI:
                 self.footer_text_left, True, self.colours["text"])
         footer_right = self.fonts["footer"].render(
                 self.footer_text_right, True, self.colours["text"])
+
+        self.screen.blit(self.viewport, self.viewport_pos)
 
         # top and bottom lines are always static
         pygame.draw.line(self.screen, self.colours["lines"],
@@ -67,11 +95,38 @@ class SickGUI:
             footer_right.get_rect().w, self.screen.get_rect().h -
             footer_right.get_rect().h))
 
+        self.render_menu()
+
         pygame.display.flip()
 
+    def render_menu(self):
 
-    def build_menu(self):
-        items = ["test123", "lalalalalala", "kjhfdkjhfkdsj"];
+        items_txt = ["test123", "lalalalalala", "kjhfdkjhfkdsj", "a", "b", "c", "d", "e"];
+        items_txt1 = ["test123", "lalalalalala", "kjhfdkjhfkdsj", "a", "b", "c", "d", "e"];
+        items_txt.extend(items_txt1)
+
+        items = []
+        for i in range(len(items_txt)):
+            if i == self.selected_menu_item:
+                items.append(self.fonts["title"].render(
+                    items_txt[i], True, self.colours["selected_text"]))
+            else:
+                items.append(self.fonts["title"].render(
+                    items_txt[i], True, self.colours["text"]))
+
+        start_item = 0 # XXX scrolling
+        for i in range(start_item, len(items)):
+
+            if (self.selected_menu_item == i):
+                r = pygame.Rect((0, i * items[i].get_rect().h),
+                        (self.screen.get_rect().w, items[i].get_rect().h))
+                pygame.draw.rect(self.viewport, self.colours["selected_bg"], r)
+
+            y = self.viewport_pos[1] + (i * items[i].get_rect().h)
+            self.screen.blit(items[i], (0, y))
+
+            if (len(items) - start_item - 1) == self.n_menu_items:
+                break
 
     def handle_events(self):
             for e in pygame.event.get():
@@ -80,11 +135,14 @@ class SickGUI:
                 elif e.type == pygame.KEYDOWN:
                     if e.key == pygame.K_ESCAPE:
                         sys.exit()
+                    elif e.key == pygame.K_DOWN:
+                        self.selected_menu_item = self.selected_menu_item + 1
+                    elif e.key == pygame.K_UP:
+                        self.selected_menu_item = self.selected_menu_item - 1
 
     def run(self):
 
         while True:
-
             self.handle_events()
             self.update_gui()
 
